78f9a557cf866f2288afc082c5a35e941b6afdb6
topherpedersen <15feetaway>
2018-02-28 14:45:00 -0600

    app now deletes 1000 numbers at a time

--- app/src/main/java/com/phponacid/android/phreakphone/BlockNumberActivity.java
+++ app/src/main/java/com/phponacid/android/phreakphone/BlockNumberActivity.java
@@ -1,12 +1,14 @@
 package com.phponacid.android.phreakphone;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.BlockedNumberContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.math.BigInteger;
@@ -34,14 +36,14 @@ public class BlockNumberActivity extends AppCompatActivity {
         String longFormDummyNumberStr;
         Uri uri;
 
-        for (int i = 0; i <= 500000; i++) {
+        for (int i = 0; i <= 10000; i++) {
             values = new ContentValues();
             j = Integer.toString(i);
             k = new BigInteger(j);
             dummyNumber = bigNumber.add(k);
             dummyNumberStr = dummyNumber.toString();
             longFormDummyNumberStr = "+1" + dummyNumberStr;
-            if (i == 500000) {
+            if (i == 10000) {
                 values.put(BlockedNumberContract.BlockedNumbers.COLUMN_ORIGINAL_NUMBER, phoneNumberStr);
                 values.put(BlockedNumberContract.BlockedNumbers.COLUMN_E164_NUMBER, longFormNumberStr);
             } else {
@@ -50,11 +52,34 @@ public class BlockNumberActivity extends AppCompatActivity {
             }
             uri = getContentResolver().insert(BlockedNumberContract.BlockedNumbers.CONTENT_URI, values);
         }
-        Toast.makeText(getApplicationContext(), "500,000 Numbers BLOCKED?",
+        Toast.makeText(getApplicationContext(), "10,000 Numbers BLOCKED?",
                 Toast.LENGTH_LONG).show();
     }
 
+    // function unblocks 1000 numbers from the users BlockedNumbers list
     public void unBlockFunction(View v) {
+
+        Cursor c = getContentResolver().query(BlockedNumberContract.BlockedNumbers.CONTENT_URI,
+                new String[]{BlockedNumberContract.BlockedNumbers.COLUMN_ID, BlockedNumberContract.BlockedNumbers.COLUMN_ORIGINAL_NUMBER,
+                        BlockedNumberContract.BlockedNumbers.COLUMN_E164_NUMBER}, null, null, null);
+        String blockedPhoneNumber;
+        ContentValues values;
+        Uri uri;
+        Integer index = 1;
+        c.moveToFirst();
+        if (c != null) {
+            do {
+                blockedPhoneNumber = c.getString(c.getColumnIndex(BlockedNumberContract.BlockedNumbers.COLUMN_ORIGINAL_NUMBER));
+                values = new ContentValues();
+                values.put(BlockedNumberContract.BlockedNumbers.COLUMN_ORIGINAL_NUMBER, blockedPhoneNumber);
+                uri = getContentResolver().insert(BlockedNumberContract.BlockedNumbers.CONTENT_URI, values);
+                getContentResolver().delete(uri, null, null);
+                index++;
+            } while (c.moveToNext() && index <= 1000);
+        }
+        Toast.makeText(getApplicationContext(), "1,000 Numbers UN-BLOCKED?",
+                Toast.LENGTH_LONG).show();
+        /*
         EditText phoneNumberEditText = (EditText) findViewById(R.id.phoneNumberEditText);
         String phoneNumberStr;
         phoneNumberStr = phoneNumberEditText.getText().toString();
@@ -69,14 +94,14 @@ public class BlockNumberActivity extends AppCompatActivity {
         String longFormDummyNumberStr;
         Uri uri;
 
-        for (int i = 0; i <= 500000; i++) {
+        for (int i = 0; i <= 10000; i++) {
             values = new ContentValues();
             j = Integer.toString(i);
             k = new BigInteger(j);
             dummyNumber = bigNumber.add(k);
             dummyNumberStr = dummyNumber.toString();
             longFormDummyNumberStr = "+1" + dummyNumberStr;
-            if (i == 500000) {
+            if (i == 10000) {
                 values.put(BlockedNumberContract.BlockedNumbers.COLUMN_ORIGINAL_NUMBER, phoneNumberStr);
                 values.put(BlockedNumberContract.BlockedNumbers.COLUMN_E164_NUMBER, longFormNumberStr);
             } else {
@@ -86,8 +111,9 @@ public class BlockNumberActivity extends AppCompatActivity {
             uri = getContentResolver().insert(BlockedNumberContract.BlockedNumbers.CONTENT_URI, values);
             getContentResolver().delete(uri, null, null);
         }
-        Toast.makeText(getApplicationContext(), "500,000 Numbers BLOCKED?",
+        Toast.makeText(getApplicationContext(), "10,000 Numbers BLOCKED?",
                 Toast.LENGTH_LONG).show();
+        */
     }
 
 }
